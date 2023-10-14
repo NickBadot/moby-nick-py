07614facfe4bcb739995ff3d1fd1cd7a0abfedd9
NickBadot <nciolas.smith1993@gmail.com>
2023-10-14 17:01:41 +0200

    Updated the sanity test to search for pictures of walruses instead Added simple assert method  for title containing string

--- SeleniumPractice/selenium_testing_0.py
+++ SeleniumPractice/selenium_testing_0.py
@@ -3,15 +3,23 @@ from selenium.webdriver.common.by import By
 import time
 
 
-def verify_driver_works():
+def search_for_pictures_of_walruses():
     driver = webdriver.Chrome()   # driver stored locally, no need for path
     driver.get('http://www.google.com/');
     time.sleep(2)  # Let the user actually see something!
     search_box = driver.find_element(By.CSS_SELECTOR, '[name="q"]')
-    search_box.send_keys('Hello  Selenium')
+    search_box.send_keys('Pictures of Walruses')
     search_box.submit()
     time.sleep(3)
     driver.quit()
+
+
+def test_title_contains(page, title):
+    driver = webdriver.Chrome()
+    driver.get(page)
+    print("Print driver: %s" % driver)
+    print("\nTitle is %s" % driver.title)
+    assert title in driver.title
 
 
 def list_elements_by_page_and_tag(page, tag):
@@ -28,8 +36,8 @@ def list_elements_by_page_and_tag(page, tag):
 
 
 def main():
-    verify_driver_works()
-    list_elements_by_page_and_tag('https://xkcd.com', 'div')
+    search_for_pictures_of_walruses()
+    test_title_contains('https://xkcd.com', 'Sign')
 
 
 if __name__ == "__main__":
