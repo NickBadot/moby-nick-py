49202068e33c7b2a1d3a77dd7250deaaa761525f
NickBadot <nciolas.smith1993@gmail.com>
2023-10-15 18:58:18 +0200

    New methods in selenium practice, messing around with the XKCD web page

--- SeleniumPractice/selenium_testing_0.py
+++ SeleniumPractice/selenium_testing_0.py
@@ -35,9 +35,73 @@ def list_elements_by_page_and_tag(page, tag):
     driver.quit()
 
 
+def slogan_search_xkcd():
+    # This method plays around with different ways to find the Slogan on the XKCD page using find_element
+    # The slogan contains the word 'sarcasm' so we asset this string mathc to validate our result as well as
+    # printing the HTML.
+
+    driver = webdriver.Chrome()
+    driver.get('https://xkcd.com')
+
+    # Find the slogan by ID, it should have a unique ID
+    slogan = driver.find_element(By.ID, 'slogan')
+    print(slogan.get_attribute("outerHTML"))
+    assert "sarcasm" in slogan.text
+
+    # here we use a CSS selector to find the slogan, selecting from a tag span with a value slogan
+    slogan = driver.find_element(By.CSS_SELECTOR, "span#slogan")
+    print(slogan.get_attribute("outerHTML"))
+    assert "sarcasm" in slogan.text
+
+    # The next three checks are CSS selectors using string search on the ID value
+    # By prefix using ^=
+    slogan = driver.find_element(By.CSS_SELECTOR, "span[id^='slog']")
+    print(slogan.get_attribute("outerHTML"))
+    assert "sarcasm" in slogan.text
+
+    # By suffix  using $=
+    slogan = driver.find_element(By.CSS_SELECTOR, "span[id$='ogan']")
+    print(slogan.get_attribute("outerHTML"))
+    assert "sarcasm" in slogan.text
+
+    # By substring using *=
+    slogan = driver.find_element(By.CSS_SELECTOR, "span[id*='loga']")
+    print(slogan.get_attribute("outerHTML"))
+    assert "sarcasm" in slogan.text
+
+    # Here, we locate the slogan via relative XPATH - using absolute XPATH would look like:
+    # /html/body/div[1]/div[2]/div[1]/span[2] and would break with any change to site structure
+    slogan = driver.find_element(By.XPATH, '//*[@id="slogan"]')
+    print(slogan.get_attribute("outerHTML"))
+    assert "sarcasm" in slogan.text
+
+    driver.quit()
+
+
+def got_to_buttersafe_from_xkcd():
+    driver = webdriver.Chrome()
+    driver.get('https://xkcd.com')
+
+    # Find the link via partial string match
+    buttersafe_link = driver.find_element(By.PARTIAL_LINK_TEXT, 'Butter')
+    time.sleep(2)
+    buttersafe_link.click()
+    time.sleep(2)
+    driver.back()
+
+    # Find the link via CSS selector, substring match
+    buttersafe_link = driver.find_element(By.CSS_SELECTOR, "a[href*='uttersaf']")
+    time.sleep(2)
+    buttersafe_link.click()
+    time.sleep(2)
+    driver.back()
+
+    driver.quit()
+
+
 def main():
-    search_for_pictures_of_walruses()
-    test_title_contains('https://xkcd.com', 'Sign')
+    slogan_search_xkcd()
+    got_to_buttersafe_from_xkcd()
 
 
 if __name__ == "__main__":
